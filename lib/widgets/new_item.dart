5a5e7be0e4f6f89456400f4bb8e53f1efee22a00
AliKhachab <a.khachab620@gmail.com>
2025-12-02 11:49:37 -0500

    added form to save user input data

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -9,91 +10,130 @@ class NewItem extends StatefulWidget {
   State<NewItem> createState() => _NewItemState();
 }
 
-  @override
-  class _NewItemState extends State<NewItem>{
+@override
+class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>(); // instead of using controllers, we use a form key to manage form state
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
     
-    void _showDialog(String message) {
-    showPlatformDialog(
-      context: context,
-      builder: (ctx) => PlatformAlertDialog(
-        title: const Text('Invalid Input'),
-        content: Text(message),
-        actions: [
-          PlatformDialogAction(
-            child: const Text('Okay'),
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-          ),
-        ],
-      ),
-    );
+    // take the form, check the state, and check if any fields are invalid. we need to put ! b/c what if state is null? in our case it won't ever be null because the form will always be there when this method is called
+    if (!_formKey.currentState!.validate()) {
+      _formKey.currentState?.save(); // saves the form state, triggering onSaved callbacks if any
     }
+  }
+
+  // void _showDialog(String message) {
+  //   showPlatformDialog(
+  //     context: context,
+  //     builder: (ctx) => PlatformAlertDialog(
+  //       title: const Text('Invalid Input'),
+  //       content: Text(message),
+  //       actions: [
+  //         PlatformDialogAction(
+  //           child: const Text('Okay'),
+  //           onPressed: () {
+  //             Navigator.of(ctx).pop();
+  //           },
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add new item"),
-      ),
-      body: Padding(padding: EdgeInsets.all(12),
-      child: Form(
+      appBar: AppBar(title: Text("Add new item")),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
                 maxLength: 50,
                 decoration: InputDecoration(label: Text('Name')),
-                validator: (value){
-                  if (value == null || value.trim().isEmpty || value.trim().length > 50) {
+                validator: (value) {
+                  if (value == null ||
+                      value.trim().isEmpty ||
+                      value.trim().length > 50) {
                     return 'Please enter a name up to 50 characters long.';
                   }
                   return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
                 },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      label:Text("Quantity"),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text("Quantity")),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            int.tryParse(value) == null ||
+                            int.parse(value) <= 0) {
+                              return 'Please enter a valid positive number.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
+                      initialValue: '1',
                     ),
-                    initialValue: '1',
                   ),
-                ),
-                const SizedBox(width:8),
-                Expanded(
-                  child: DropdownButtonFormField(items: [
-                    for(final category in categories.entries)
-                      DropdownMenuItem(
-                        value:category.value,
-                        child: Row(children: [
-                        Container(
-                          width:16, 
-                          height:16,
-                          color:category.value.color
-                        ),
-                        const SizedBox(width:6),
-                        Text(category.value.name),
-                      ],)
-                      ,)
-                  ], onChanged: (value){}),
-                ),
-              ],),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      initialValue: _selectedCategory,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {
+                        setState(() => _selectedCategory = value!);
+                      },
+                    ),
+                  ),
+                ],
+              ),
               const SizedBox(height: 12),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: (){}, child: Text("Reset"), ),
-                  ElevatedButton(onPressed: (){}, child: Text("Add Item"), ),
-
-              ],)
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: Text("Reset"),
+                  ),
+                  ElevatedButton(onPressed: _saveItem, child: Text("Add Item")),
+                ],
+              ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
